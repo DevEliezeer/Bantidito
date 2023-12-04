8ec8cde5556a30e47935bb5018ab82fcf1a6047c
PC <a>
2023-12-03 20:43:52 -0500

    Initial commit

---
+++ src/main/java/dev/vanitymc/bantidito/Bantidito.java
@@ -0,0 +1,17 @@
+package dev.vanitymc.bantidito;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bantidito extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
